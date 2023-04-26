7ab6913eef82b54fa3d4b1947474f3d687a03446
Panikiran <panikiranvenkatesh.periyapatna@externals.hilti.com>
2023-04-26 10:14:23 +0200

    Introduce Flyway with basic table structure

--- src/main/java/com/inditex/pricecalculator/service/PriceQueryService.java
+++ src/main/java/com/inditex/pricecalculator/service/PriceQueryService.java
@@ -3,10 +3,12 @@ package com.inditex.pricecalculator.service;
 import com.inditex.pricecalculator.dto.PriceQueryRequestDto;
 import com.inditex.pricecalculator.dto.PriceQueryResponseDto;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 @Slf4j
 public class PriceQueryService {
 
---
+++ src/main/resources/migration/V1__init.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS brand;
+DROP TABLE IF EXISTS prices;
+
+DROP SEQUENCE IF EXISTS product_seq;
+DROP SEQUENCE IF EXISTS brand_seq;
+DROP SEQUENCE IF EXISTS prices_seq;
+
+CREATE SEQUENCE product_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE CACHE 1;
+
+CREATE SEQUENCE brand_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE CACHE 1;
+
+CREATE SEQUENCE prices_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE CACHE 1;
+
+
+CREATE TABLE product
+(
+    id           bigint NOT NULL DEFAULT NEXTVAL('product_seq'),
+    product_id   bigint NOT NULL,
+    product_name character varying(255),
+    CONSTRAINT product_pk_id PRIMARY KEY (id)
+);
+
+CREATE TABLE brand
+(
+    id         bigint NOT NULL DEFAULT NEXTVAL('brand_seq'),
+    brand_id   bigint NOT NULL,
+    brand_name character varying(255),
+    CONSTRAINT brand_pk_id PRIMARY KEY (id)
+);
+
+CREATE TABLE prices
+(
+    id         bigint         NOT NULL DEFAULT NEXTVAL('prices_seq'),
+    price_list bigint         NOT NULL,
+    priority   bigint         NOT NULL,
+    price      decimal(10, 2) NOT NULL,
+    curr       character varying(3),
+    start_date timestamp without time zone,
+    end_date   timestamp without time zone,
+);
